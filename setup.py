17f94e30d8ec7b2d661b640608ac2c7b42ee7883
Rafael O <79538153+rafaotaiza@users.noreply.github.com>
2023-03-29 12:16:54 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from Mod01_Waves_JROP import __version__
+
+setup(
+    name='Mod01_Waves_JROP',
+    version=__version__,
+
+    url='https://github.com/rafaotaiza/VBlast_Mod01',
+    author='J. Rafael Otaíza P.',
+    author_email='rafaotaiza@gmail.com',
+
+    py_modules=['Mod01_Waves_JROP'],
+)
